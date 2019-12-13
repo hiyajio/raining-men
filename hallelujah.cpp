cd2ef1b9b96f9fc319f0c5616ebd1bda2d4116c4
lockjio <jio.buenviaje@gmail.com>
2019-12-13 18:14:07 -0500

    Add description and libraries

---
+++ hallelujah.cpp
@@ -0,0 +1,11 @@
+/* Developer: Juan Sergio Buenviaje
+ * Lab: Tuesdays 2:25 - 3:15pm
+ * Program: hallelujah.cpp
+ * Description: Inspired by the 1980's classic song "It's Raining Men" by The Weather Girls, this game is essentially what that would look like in reality (Best played when song is also playing in the background).
+ */
+#include <iostream>
+#include "gfx.h"
+#include <unistd.h>
+#include <cstdlib>
+#include <math.h>
+#include <vector>
