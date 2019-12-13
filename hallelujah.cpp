de852f83038f0c3fd4fdf75ec390606c6252e9f5
lockjio <jio.buenviaje@gmail.com>
2019-12-13 18:47:09 -0500

    Make man move

--- hallelujah.cpp
+++ hallelujah.cpp
@@ -9,3 +9,76 @@
 #include <cstdlib>
 #include <math.h>
 #include <vector>
+
+void man(const int, int, int);
+void raining_man(const int, int, int);
+bool impact(int circleX, int circleY, int x, int y);
+
+#define PI 3.14159265
+
+int main()
+{
+    const int XSIZE = 700, YSIZE = 500, SIZE = 50, BIGCIRCLER = 300, SMLCIRCLER = 250, TOPPOS = 49, BOTPOS = 451, RADIUS = 50;
+    unsigned int delay = 10000;
+    char c;
+    int x = 350, y = 450, response;
+    int counter = 0;
+    int i = 72;
+
+    gfx_open(XSIZE, YSIZE, "It's Raining Men!"); // Open a new window for drawing.
+    gfx_color(255, 255, 255);
+    gfx_text(25, 225, "Use the left and right arrow keys to move the man.");
+    gfx_text(175, 235, "Avoid... the raining men! Hallelujah!");
+    man(RADIUS, x, TOPPOS); // Make watch
+
+    c = gfx_wait();
+    gfx_flush();
+
+    while (true)
+    {
+        gfx_clear();
+        c = gfx_wait();
+        man(SIZE, x, y); // Make watch
+
+        if (c == 'Q') // On left click, change direction of hand
+        {
+            x = x - 25;
+            man(SIZE, x, y);
+        }
+        else if (c == 'S')
+        {
+            x = x + 25;
+            man(SIZE, x + 25, y);
+        }
+        else if (c == 'q')
+        {
+            break; // Quit if it is the letter q.
+        }
+        usleep(delay);
+    }
+}
+
+void man(const int SIZE, int x, int y)
+{
+    gfx_color(0, 200, 200);
+    gfx_line((x - (SIZE / 2)), (y + (SIZE / 2)), (x + (SIZE / 2)), (y + (SIZE / 2)));
+    gfx_line((x + (SIZE / 2)), (y + (SIZE / 2)), (x + (SIZE / 2)), (y - (SIZE / 2)));
+    gfx_line((x - (SIZE / 2)), (y - (SIZE / 2)), (x + (SIZE / 2)), (y - (SIZE / 2)));
+    gfx_line((x - (SIZE / 2)), (y + (SIZE / 2)), (x - (SIZE / 2)), (y - (SIZE / 2)));
+    gfx_circle(x, y - SIZE, SIZE / 2);
+    gfx_circle(x, y - SIZE, SIZE / 2);
+}
+
+void raining_man(const int SIZE, int x, int y)
+{
+    gfx_color(0, 200, 200);
+    gfx_circle(x, y, SIZE / 2);
+}
+
+// // Checks to see if there was an impact
+// bool impact(int circleX, int circleY, int x, int y)
+// {
+//     if (x <= circleX + targetRadius && x >= circleX - targetRadius && y <= circleY + targetRadius && y >= circleY - targetRadius)
+//         return true;
+//     return false;
+// }
