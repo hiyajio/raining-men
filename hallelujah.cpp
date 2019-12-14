5ce48a72ff7107909545bf681ac1e8c1fb54f23b
lockjio <jio.buenviaje@gmail.com>
2019-12-13 19:29:15 -0500

    Clean start menu

--- hallelujah.cpp
+++ hallelujah.cpp
@@ -6,20 +6,21 @@
 #include <iostream>
 #include "gfx.h"
 #include <unistd.h>
+#include <stdlib.h>
 #include <cstdlib>
 #include <math.h>
 #include <vector>
 
 void man(const int, int, int);
 void raining_man(const int, int, int);
-void menu(const int, int, int, char);
-bool impact(int circleX, int circleY, int x, int y);
+void menu(const int, const int, const int, int, int, char);
+bool impact(int, int, int, int);
 
 #define PI 3.14159265
 
 int main()
 {
-    const int XSIZE = 700, YSIZE = 500, SIZE = 50, MENUSML = 49, MENULRG = 451, RADIUS = 25;
+    const int XSIZE = 700, YSIZE = 500, SIZE = 50, RADIUS = 25;
     unsigned int delay = 10000;
     char c;
     int x = 350, y = 450, response;
@@ -30,7 +31,7 @@ int main()
     gfx_open(XSIZE, YSIZE, "It's Raining Men!"); // Open a new window for drawing.
     gfx_color(200, 200, 0);
 
-    menu(SIZE, x, y, c);
+    menu(SIZE, XSIZE, YSIZE, x, y, c);
 
     while (true)
     {
@@ -88,18 +89,29 @@ void raining_man(const int SIZE, int x, int y)
     gfx_circle(x, y - SIZE, SIZE / 2);
 }
 
-void menu(const int SIZE, int x, int y, char c)
+void menu(const int SIZE, const int XSIZE, const int YSIZE, int x, int y, char c)
 {
-    gfx_line((x - (SIZE / 2)), (y + (SIZE / 2)), (x + (SIZE / 2)), (y + (SIZE / 2)));
-    gfx_line((x + (SIZE / 2)), (y + (SIZE / 2)), (x + (SIZE / 2)), (y - (SIZE / 2)));
-    gfx_line((x - (SIZE / 2)), (y - (SIZE / 2)), (x + (SIZE / 2)), (y - (SIZE / 2)));
-    gfx_line((x - (SIZE / 2)), (y + (SIZE / 2)), (x - (SIZE / 2)), (y - (SIZE / 2)));
-    gfx_text(200, 200, "| Use the left and right arrow keys to move the man. |");
-    gfx_text(240, 225, "| Avoid... the raining men! Hallelujah! |");
+    gfx_line(150, 150, 550, 150);
+    gfx_line(550, 150, 550, 300);
+    gfx_line(550, 300, 150, 300);
+    gfx_line(150, 300, 150, 150);
+    gfx_color(255, 255, 255);
+    gfx_line(175, 175, 525, 175);
+    gfx_line(525, 175, 525, 275);
+    gfx_line(525, 275, 175, 275);
+    gfx_line(175, 275, 175, 175);
+    gfx_color(200, 200, 0);
+    gfx_text(200, 200, "Use the left and right arrow keys to move the man.");
+    gfx_text(240, 225, "Avoid... the raining men! Hallelujah!");
     man(SIZE, x, y); // Make watch
 
     c = gfx_wait();
     gfx_flush();
+
+    if (c == 'q')
+    {
+        exit(0);
+    }
 }
 
 // // Checks to see if there was an impact
