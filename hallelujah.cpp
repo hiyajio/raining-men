f042e5312abef883253572fb2205ebfeaa341af6
lockjio <jio.buenviaje@gmail.com>
2019-12-13 19:09:03 -0500

    Refactor menu function

--- hallelujah.cpp
+++ hallelujah.cpp
@@ -12,49 +12,57 @@
 
 void man(const int, int, int);
 void raining_man(const int, int, int);
+void menu(const int, int, int, char);
 bool impact(int circleX, int circleY, int x, int y);
 
 #define PI 3.14159265
 
 int main()
 {
-    const int XSIZE = 700, YSIZE = 500, SIZE = 50, BIGCIRCLER = 300, SMLCIRCLER = 250, TOPPOS = 49, BOTPOS = 451, RADIUS = 50;
+    const int XSIZE = 700, YSIZE = 500, SIZE = 50, MENUSML = 49, MENULRG = 451, RADIUS = 25;
     unsigned int delay = 10000;
     char c;
     int x = 350, y = 450, response;
     int counter = 0;
     int i = 72;
+    bool play = true;
 
     gfx_open(XSIZE, YSIZE, "It's Raining Men!"); // Open a new window for drawing.
     gfx_color(200, 200, 0);
-    gfx_text(200, 200, "Use the left and right arrow keys to move the man.");
-    gfx_text(240, 225, "Avoid... the raining men! Hallelujah!");
-    man(SIZE, x, y); // Make watch
 
-    c = gfx_wait();
-    gfx_flush();
+    menu(SIZE, x, y, c);
 
     while (true)
     {
-        gfx_clear();
-        c = gfx_wait();
+        response = gfx_event_waiting();
         man(SIZE, x, y); // Make watch
 
-        if (c == 'Q') // On left click, change direction of hand
+        if (response)
         {
-            x = x - 25;
-            man(SIZE, x, y);
+            gfx_clear();
+            c = gfx_wait();
+            if (c == 'Q') // On left click, change direction of hand
+            {
+                x = x - 25;
+                man(SIZE, x, y);
+            }
+            else if (c == 'S')
+            {
+                x = x + 25;
+                man(SIZE, x, y);
+            }
+            else if (c == 'q')
+            {
+                break; // Quit if it is the letter q.
+            }
+            usleep(delay);
         }
-        else if (c == 'S')
+        else
         {
-            x = x + 25;
-            man(SIZE, x, y);
+            gfx_clear();
+            man(SIZE, x, y); // Make watch
+            usleep(delay);
         }
-        else if (c == 'q')
-        {
-            break; // Quit if it is the letter q.
-        }
-        usleep(delay);
     }
 }
 
@@ -72,7 +80,26 @@ void man(const int SIZE, int x, int y)
 void raining_man(const int SIZE, int x, int y)
 {
     gfx_color(0, 200, 200);
-    gfx_circle(x, y, SIZE / 2);
+    gfx_line((x - (SIZE / 2)), (y + (SIZE / 2)), (x + (SIZE / 2)), (y + (SIZE / 2)));
+    gfx_line((x + (SIZE / 2)), (y + (SIZE / 2)), (x + (SIZE / 2)), (y - (SIZE / 2)));
+    gfx_line((x - (SIZE / 2)), (y - (SIZE / 2)), (x + (SIZE / 2)), (y - (SIZE / 2)));
+    gfx_line((x - (SIZE / 2)), (y + (SIZE / 2)), (x - (SIZE / 2)), (y - (SIZE / 2)));
+    gfx_circle(x, y - SIZE, SIZE / 2);
+    gfx_circle(x, y - SIZE, SIZE / 2);
+}
+
+void menu(const int SIZE, int x, int y, char c)
+{
+    gfx_line((x - (SIZE / 2)), (y + (SIZE / 2)), (x + (SIZE / 2)), (y + (SIZE / 2)));
+    gfx_line((x + (SIZE / 2)), (y + (SIZE / 2)), (x + (SIZE / 2)), (y - (SIZE / 2)));
+    gfx_line((x - (SIZE / 2)), (y - (SIZE / 2)), (x + (SIZE / 2)), (y - (SIZE / 2)));
+    gfx_line((x - (SIZE / 2)), (y + (SIZE / 2)), (x - (SIZE / 2)), (y - (SIZE / 2)));
+    gfx_text(200, 200, "| Use the left and right arrow keys to move the man. |");
+    gfx_text(240, 225, "| Avoid... the raining men! Hallelujah! |");
+    man(SIZE, x, y); // Make watch
+
+    c = gfx_wait();
+    gfx_flush();
 }
 
 // // Checks to see if there was an impact
